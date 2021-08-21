b51e2fc6b399794c207a9f357883bc8f03eac92a
itabib <36846870+itabib@users.noreply.github.com>
2021-08-21 03:05:58 +0300

    initial project

---
+++ src/test/java/UserAPIValidation/UserAPIAssignment/UserAPITest.java
@@ -0,0 +1,14 @@
+package UserAPIValidation.UserAPIAssignment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class UserAPITest {
+  @Test
+  public void UserAPIValidation() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
